a1e0648c60c12f274b665fcac056825bb155631f
alexi <alexi@ALEXANDER>
2023-09-21 23:51:29 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PromNotas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el número de notas: ");
+            int cantdNts = int.Parse(Console.ReadLine());
+
+            int SumaNts = 0;
+
+            for (int i = 1; i <= cantdNts; i++)
+            {
+                Console.Write($"Ingrese la nota {i}: ");
+                int Nota = int.Parse(Console.ReadLine());
+                SumaNts += Nota;
+            }
+
+            int Promedio = SumaNts / cantdNts;
+
+            Console.WriteLine($"El promedio es {Promedio}");
+        }
+    }
+}
